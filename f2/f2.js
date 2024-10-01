042304f3d052c2965213762d13e2dd7bf0301e5b
rae <krstfboda@gmail.com>
2024-10-01 09:10:58 +0200

    kesz

--- f2/f2.js
+++ f2/f2.js
@@ -2,7 +2,16 @@ const students = require('../students.json');
 /* Írj függvényt ami feltölt egy objektumot (lehetőleg üres {}-ről induljon) 
 a klubbok nevével és tagjainak számával. (pl. {"club1": 4, "club2": 3....}) */
 function f2() {
-    //TODO
+    let obj = {}
+    students.forEach(element => {
+        if (!obj[element.club]){
+            obj[`${element.club}`] = 1
+        }
+        else{
+            obj[`${element.club}`]++;
+        }
+    });
+    return obj
 }
 
 module.exports = f2;
